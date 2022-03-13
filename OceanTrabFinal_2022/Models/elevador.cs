a06f9fa691e8eebd33b23dc3e2ea67a56ccba88a
Matias Perazoli <83455769+perazoli@users.noreply.github.com>
2022-03-13 15:46:14 -0300

    Versão 1.0 Trabalho Final

---
+++ OceanTrabFinal_2022/Models/elevador.cs
@@ -0,0 +1,10 @@
+﻿namespace OceanTrabFinal_2022.Models
+{
+    internal class Elevador
+    {
+        public int Andares { get; set; } // quantidade de andares do elevador
+        public int Capacidade { get; set; } // capacidade máxima de pessoas
+        public int andarAtual { get; set; } // andar atual do elevador
+        public int pessoasAtual { get; set; } // ocupação atual
+    }
+}
